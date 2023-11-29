a2ba28319b47caba5d6c79b8ac56cd93ad8a05cc
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2023-11-29 13:15:40 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function predict() {
+    const soilType = document.getElementById('soilSelect').value
+    const crop = document.getElementById('cropSelect').value
+    const Nitrogen = document.getElementById('nitrogenValue').value
+    const Phosphorous = document.getElementById('potassiumValue').value
+    const Potassium = document.getElementById('phosphorousValue').value
+    fetch(`https://2m9vw4li1j.execute-api.ap-south-1.amazonaws.com/default/fertilizers?soilType=${soilType}&cropType=${crop}&nitrogen=${Nitrogen}&potassium=${Potassium}&phosphorous=${Phosphorous}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        document.getElementById('result').innerText = data.prediction + " fertilizer"
+      })
+      .catch(error => console.error('There was a problem with the fetch operation:', error));
+  }
+  // JavaScript to handle +/- buttons
+  $(document).ready(function () {
+    $(".plus").click(function () {
+      var numberElement = $(this).siblings(".input-field");
+      var currentNumber = parseFloat(numberElement.val());
+      numberElement.val((currentNumber + 1));
+    });
+  
+    $(".minus").click(function () {
+      var numberElement = $(this).siblings(".input-field");
+      var currentNumber = parseFloat(numberElement.val());
+      if (currentNumber > 0) {
+        numberElement.val((currentNumber - 1));
+      }
+    });
+  });
