51f6732aef091c56684aa5e2622152d4547b017e
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2023-11-29 13:28:13 +0530

    commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function predict() {
-  document.getElementById('result').innerText = ""
+  document.getElementById('result').style.backgroundColor="rgb(208 242 136 / 73%)"
   const soilType = document.getElementById('soilSelect').value
   const crop = document.getElementById('cropSelect').value
   const Nitrogen = document.getElementById('nitrogenValue').value
   const Phosphorous = document.getElementById('potassiumValue').value
   const Potassium = document.getElementById('phosphorousValue').value
   fetch(`https://2m9vw4li1j.execute-api.ap-south-1.amazonaws.com/default/fertilizers?soilType=${soilType}&cropType=${crop}&nitrogen=${Nitrogen}&potassium=${Potassium}&phosphorous=${Phosphorous}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      document.getElementById('result').innerText = data.prediction + " fertilizer"
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    document.getElementById('result').innerText = data.prediction + " fertilizer"
+    document.getElementById('result').style.backgroundColor="transparent"
     })
     .catch(error => console.error('There was a problem with the fetch operation:', error));
 }
